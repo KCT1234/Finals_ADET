13bd9057e4f7f9011a8a834700155ab17aa3b889
CSiron21 <csiron9029@gmail.com>
2024-10-21 22:03:08 +0800

    Updated drawer_controller.dart
    changed text and icon colors to maroon. also, removed box shadows for seamless button

--- lib/controllers/drawer_controller.dart
+++ lib/controllers/drawer_controller.dart
@@ -17,33 +17,25 @@ class drawerController extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      splashColor: Colors.red[100],
+      splashColor: Color.fromARGB(255, 130, 35, 35),
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         margin: const EdgeInsets.symmetric(vertical: 4),
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: Color.fromARGB(255, 130, 35, 35),
           borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              spreadRadius: 2,
-              blurRadius: 4,
-              offset: const Offset(0, 2), // changes position of shadow
-            ),
-          ],
         ),
         child: Row(
           children: [
             Icon(
               icon,
-              color: Colors.black,
+              color: Colors.white,
             ),
             const SizedBox(width: 16),
             Text(
               text,
               style: const TextStyle(
-                color: Colors.black,
+                color: Colors.white,
               ),
             ),
           ],
