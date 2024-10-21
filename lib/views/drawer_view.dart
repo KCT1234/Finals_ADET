82d3fcbfc153939bccce248a9f97b631c6208c1e
CSiron21 <csiron9029@gmail.com>
2024-10-21 22:06:08 +0800

    Update drawer_view.dart
    put switch pages function for the buttons. moved assistance_page contents to merge in help_feedback. removed map tile style from settings_page. changed background of drawer to maroon.

--- lib/views/drawer_view.dart
+++ lib/views/drawer_view.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:final_adet/views/login_page.dart';
 import 'package:final_adet/controllers/drawer_controller.dart';
+import 'package:final_adet/views/menu/inbox_page.dart';
+import 'package:final_adet/views/menu/activity_page.dart';
+import 'package:final_adet/views/menu/help_feedback.dart';
+import 'package:final_adet/views/menu/settings_page.dart';
 
 class drawerView extends StatelessWidget {
   const drawerView({super.key});
@@ -10,136 +14,153 @@ class drawerView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User? user = FirebaseAuth.instance.currentUser; // Get the current user
-    String userName = user != null ? user.email!.split('@')[0] : 'Adet Student'; // Default to 'Adet Student' if no user
-    String userEmail = user != null ? user.email! : 'adetstudent00@mail.com'; // Default email
+    String userName = user != null
+        ? user.email!.split('@')[0]
+        : 'Adet Student'; // Default to 'Adet Student' if no user
+    String userEmail =
+        user != null ? user.email! : 'adetstudent00@mail.com'; // Default email
 
     return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            color: const Color.fromARGB(255, 130, 35, 35),
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                const CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    CupertinoIcons.person,
-                    size: 40,
-                    color: Color.fromARGB(255, 130, 35, 35),
+      child: Container(
+        color:
+            Color.fromARGB(255, 130, 35, 35), // Set the background color here
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 130, 35, 35),
+                border: Border(
+                  bottom: BorderSide(
+                      color: Colors.white,
+                      width: 2), // Add white outline only at the bottom
+                ),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      CupertinoIcons.person,
+                      size: 40,
+                      color: Color.fromARGB(255, 130, 35, 35),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        userName, // Display logged-in username
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        userEmail, // Display logged-in user's email
+                        style: const TextStyle(
+                          color: Colors.white70,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  drawerController(
+                    icon: CupertinoIcons.info,
+                    text: 'Inbox',
+                    onTap: () {
+                      // Handle Inbox action
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const InboxPage()), // Navigate to Inbox page
+                      );
+                    },
+                  ),
+                  drawerController(
+                    icon: CupertinoIcons.settings,
+                    text: 'Settings',
+                    onTap: () {
+                      // Handle Settings action
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SettingsPage()),
+                      );
+                    },
+                  ),
+                  drawerController(
+                    icon: CupertinoIcons.clock,
+                    text: 'Activity',
+                    onTap: () {
+                      // Handle Activity action
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const ActivityPage()), // Navigate to Activity page
+                      );
+                    },
+                  ),
+                  drawerController(
+                    icon: CupertinoIcons.question_circle,
+                    text: 'Help and Feedback',
+                    onTap: () {
+                      // Handle Help and Feedback action
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const HelpFeedbackPage()), // Navigate to Help Feedback page
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut(); // Sign out the user
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const LoginScreen()), // Redirect to login page
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red[700],
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  elevation: 5,
+                ),
+                child: const Text(
+                  'Log Out',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
                   ),
                 ),
-                const SizedBox(width: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      userName, // Display logged-in username
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      userEmail, // Display logged-in user's email
-                      style: const TextStyle(
-                        color: Colors.white70,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                drawerController(
-                  icon: CupertinoIcons.info,
-                  text: 'Inbox',
-                  onTap: () {
-                    // Handle Inbox action
-                  },
-                ),
-                drawerController(
-                  icon: CupertinoIcons.settings,
-                  text: 'Settings',
-                  onTap: () {
-                    // Handle Settings action
-                  },
-                ),
-                drawerController(
-                  icon: CupertinoIcons.bell,
-                  text: 'Notifications',
-                  onTap: () {
-                    // Handle Notifications action
-                  },
-                ),
-                drawerController(
-                  icon: CupertinoIcons.question_circle,
-                  text: 'Help and Feedback',
-                  onTap: () {
-                    // Handle Help and Feedback action
-                  },
-                ),
-                drawerController(
-                  icon: CupertinoIcons.clock,
-                  text: 'Activity',
-                  onTap: () {
-                    // Handle Activity action
-                  },
-                ),
-                drawerController(
-                  icon: CupertinoIcons.placemark,
-                  text: 'Saved Places',
-                  onTap: () {
-                    // Handle Saved Places action
-                  },
-                ),
-                drawerController(
-                  icon: CupertinoIcons.phone,
-                  text: 'Assistance',
-                  onTap: () {
-                    // Handle Assistance action
-                  },
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut(); // Sign out the user
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginScreen()), // Redirect to login page
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red[700],
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                elevation: 5,
-              ),
-              child: const Text(
-                'Log Out',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
